f8967755523932c91602d8527088bfc2cf1e82d5
Frederico131324 <fredericosilva.201701@epff-intep.pt>
2024-09-16 17:18:23 +0100

    Aula1

---
+++ aula1/src/App.java
@@ -0,0 +1,13 @@
+import java.text.DecimalFormat;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome = "Manuel";
+        String morada = "Avenida sem numero";
+        int idade = 63;
+        char estadocivil = 'C';
+        double mediahoras = 16.5;
+
+        System.out.println("Nome: " + nome);
+    }
+}
